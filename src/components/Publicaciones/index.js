956338c25502a8c5ad3b1265c16ce48563119724
Martin Gonzalez <marttingonzalez@gmail.com>
2021-06-26 14:14:55 -0300

    se agregan actions y reducers para traer publicaciones de usuarios

--- src/components/Publicaciones/index.js
+++ src/components/Publicaciones/index.js
@@ -1,16 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as usuariosActions from "../../actions/usuariosActions";
+import * as publicacionesActions from "../../actions/publicacionesActions";
 
+//Los destructuro y renombro al mismo tiempo:
+const { traerTodos: usuariosTraerTodos} = usuariosActions; 
+const { traerPorUsuario: publicacionesTraerPorUsuario} = publicacionesActions;
 
 class Publicaciones extends Component {
-    render() {
 
-        return (
-            
-            <div>
-               {`Hola ${this.props.match.params.key}`}
-            </div>
-        )
+  async componentDidMount() {
+    //Si no tiene usuarios, primero los cargo y cuando termina cargo las publicaciones
+    if (!this.props.usuariosReducer.usuarios.lenght) {
+     await this.props.usuariosTraerTodos();
     }
+    const key = this.props.match.params.key;
+    this.props.publicacionesTraerPorUsuario(key);
+  }
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1> Publicaciones de {}</h1>
+      </div>
+    );
+  }
 }
+const mapStateToProps = ({ usuariosReducer, publicacionesReducer }) => {
+  return {
+    usuariosReducer,
+    publicacionesReducer,
+  };
+};
+const mapDispatchToProps = {
+    usuariosTraerTodos,
+    publicacionesTraerPorUsuario,
+};
 
-export default Publicaciones;
+export default connect(mapStateToProps, mapDispatchToProps)(Publicaciones);
