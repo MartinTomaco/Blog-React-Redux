e12e14334197ecafc4c5b304f510bb232f8569c2
Martin Gonzalez <marttingonzalez@gmail.com>
2021-07-05 20:24:46 -0300

    se visualizan tareas y input type checkbox

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -8,9 +8,21 @@ export const  traerTodas = () => async (dispatch) => {
     try {
         const respuesta = await axios.get('https://jsonplaceholder.typicode.com/todos'); // tarea: To Do
 
+        // respuesta es un arreglo de objetos no separados por usuario
+        // quiero crear un nuevo objeto separado por las tareas de cada usuario
+        const tareas = {};
+        respuesta.data.map( (tar) => (
+            tareas[tar.userId] = {
+                ...tareas[tar.userId],
+                [tar.id]: {
+                    ...tar
+                }
+            }
+        ));
+
         dispatch({
             type: TRAER_TODAS,
-            payload: respuesta.data,
+            payload: tareas,
     
         })
     } catch (error) {
--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Spinner from '../General/Spinner'
+import Fatal from '../General/Fatal'
 
 import * as tareasActions from "../../actions/tareasActions";
 
@@ -8,13 +10,50 @@ const { traerTodas } = tareasActions;
 
 class Tareas extends Component {
     componentDidMount() {
-        this.props.traerTodas()
+        if (!Object.keys(this.props.tareas).length) {
+            this.props.traerTodas();
+        }
     }
+
+    mostrarContenido = () => {
+        const { tareas, cargando, error} = this.props;
+        if (cargando) {
+            return <Spinner/>
+        }
+        if (error) {
+            return <Fatal mensaje={error.mensaje}/>
+        }
+        return Object.keys(tareas).map((usu_id)=> (
+            <div key={usu_id}>
+                <h2>
+                    Usuario {usu_id}
+                </h2>
+                <div className='contenedor_tareas'>
+                    {this.ponerTareas(usu_id)}
+                </div>
+            </div>
+        ))
+    }
+    ponerTareas = (usu_id) => {
+        const { tareas } = this.props;
+        const por_usuario = {
+            ...tareas[usu_id]
+        };
+        
+        return Object.keys(por_usuario).map((tar_id) =>(
+            <div key={tar_id}>
+            <input type='checkbox' defaultChecked={por_usuario[tar_id].completed} />
+            {
+                por_usuario[tar_id].title
+            }
+            </div>
+        ));
+    };
     render() {
         console.log(this.props);
         return (
             <div>
-                Tareas
+                {this.mostrarContenido()}
             </div>
         )
     }
