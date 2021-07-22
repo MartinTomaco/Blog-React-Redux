6f56515b0d13e3e8e5e34bec4f2adf8cfd01bd14
Martin Gonzalez <marttingonzalez@gmail.com>
2021-07-22 19:43:58 -0300

    se agregan iconos al footer

--- src/components/General/Footer.js
+++ src/components/General/Footer.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import '../../css/footer.css'
+import instagram from '../../assets/instagram-brands.svg';
+import twitter from '../../assets/twitter-brands.svg';
+import github from '../../assets/github-brands.svg';
+
 
 const Footer = (props) => (
     <footer className="footer">
         <div className='redes-container'>
-            <b>social networs</b>
+            <span>social networs</span>
+            <div>
+                <img className="filter-color" src={instagram} alt="instagram"></img>
+                <img className="filter-color" src={twitter} alt="twitter"></img>  
+                <img className="filter-color" src={github} alt="github"></img>
+            </div>
+            
         </div>
         <div className='credits-container'>
-            dising by martin_tomaco
+            <span>design by martin_tomaco</span>
         </div>
     </footer>
 )
